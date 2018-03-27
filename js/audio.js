7a7e7d30c6595a10977240216925883ead729d4e
Lenovo <leno@gmail.com>
2018-03-27 19:32:49 +0800

    調整播放器-1

--- js/audio.js
+++ js/audio.js
@@ -1,7 +1,7 @@
 
 var aud;
-var nPlayStart = 500;
-var nPlayDuration = 5;
+var nPlayStart = 0;
+var nPlayDuration = 0;
 
 function onLoad() {
 	aud = document.getElementById("myAudio");
@@ -9,13 +9,10 @@ function onLoad() {
 	aud.addEventListener("timeupdate", onTimeUpdate);
 }
 
-function onPlayDurationChange(e) {
-	nPlayDuration = parseInt(e.value);
-//	alert(nPlayDuration);
-}
 
 
 function onTimeUpdate() {
+	if(nPlayDuration<1) return;
 //	document.getElementById("palyLength").value = aud.currentTime;
 	if(aud.currentTime > (nPlayStart+nPlayDuration)) {
 		aud.pause();
@@ -23,9 +20,43 @@ function onTimeUpdate() {
 	}
 }
 
+function getMS(sId) {
+	var m=0, s=0;
+	if(sId=="palyStart") {
+		m=parseInt(document.getElementById("palyStartM").value);
+		s=parseInt(document.getElementById("palyStartS").value);
+	} else {
+		m=parseInt(document.getElementById("playLengthM").value);
+		s=parseInt(document.getElementById("playLengthS").value);
+	}
+	return (m*60)+s;
+}
+
+function cusTime(nType) {
+	var t=aud.currentTime;
+	var h=Math.floor(t/3600);
+	var m=Math.floor((t % 3600)/60);
+	var s=Math.floor(t % 60);
+
+	if(nType==1){ //start
+		nPlayStart=t;
+		document.getElementById("palyStartH").value=h;
+		document.getElementById("palyStartM").value=m;
+		document.getElementById("palyStartS").value=s;
+	}else{
+nPlayDuration = aud.currentTime-nPlayStart;
+t=nPlayDuration;
+		document.getElementById("playLengthH").value=Math.floor(t/3600);
+		document.getElementById("playLengthM").value=Math.floor((t % 3600)/60);
+		document.getElementById("playLengthS").value=Math.floor(t % 60);
+	}
+}
+
 function cusPlay() {
 //	var aud = document.getElementById("myAudio");
-	aud.currentTime = document.getElementById("palyStart").value;
+	nPlayStart = getMS("palyStart");
+	nPlayDuration = getMS("playDuration");
+	aud.currentTime = nPlayStart;//document.getElementById("palyStart").value;
 	aud.play();
 }
 
